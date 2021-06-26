92804bd2f6e6d311c9b7768fb3de0762981e4bd5
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-06-26 02:52:18 +0100

    JavaScript Drum Kit

---
+++ week-one/javascript_drum_kit/script.js
@@ -0,0 +1,21 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    if (!audio) return; // stop the function from running 
+    // if we want to hit repeatedly the keys...
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+
+    key.classList.add('playing');
+
+}
+
+function removeTransition(e) {
+    if (e.propertyName != 'transform') return; // skip if it is not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
