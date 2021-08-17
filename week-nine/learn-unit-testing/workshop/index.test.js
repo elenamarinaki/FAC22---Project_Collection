92a2d47b3392047c42879e52fa655761ef84e94a
Elena <rhapsodyenmauve@gmail.com>
2021-08-17 23:45:00 +0100

    test notEqual - makeUrl

--- week-nine/learn-unit-testing/workshop/index.test.js
+++ week-nine/learn-unit-testing/workshop/index.test.js
@@ -3,3 +3,8 @@ test('makeURL should return the correct pokemon ULR', () => {
   equal(makeUrl('bulbasaur'), 'https://pokeapi.co/api/v2/bulbasaur');
   equal(makeUrl('eevwee'), 'https://pokeapi.co/api/v2/eevee');
 });
+
+test('makeUrl should return an error', () => {
+  notEqual(makeUrl('eevee'), 'https://pokeapi.co/api/v2/eevee');
+  notEqual(makeUrl('eeveeeee'), 'https://pokeapi.co/api/v2/eevee');
+});
