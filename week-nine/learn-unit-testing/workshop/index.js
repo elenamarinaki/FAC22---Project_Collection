c87d6103b467a3ad2d55927f39cb792679303941
Elena <rhapsodyenmauve@gmail.com>
2021-08-18 02:18:35 +0100

    leap year function

--- week-nine/learn-unit-testing/workshop/index.js
+++ week-nine/learn-unit-testing/workshop/index.js
@@ -10,3 +10,13 @@ function searchParamsToObject(str) {
 }
 
 // searchParamsToObject('name=oliver&email=hello@oliverjam.es');
+
+function isLeapYear(year) {
+  if (typeof year !== 'number') return `Please enter a number`;
+  if (year < 0) return `Year cannot be a negative number`;
+  if (year % 100 === 0) return `Year ${year} is not a leap year!`;
+  if (year % 400 === 0 || year % 4 === 0) {
+    return true;
+  }
+  return false;
+}
