0f5f697e4cba594c94e84b00a68637abfe2dda68
Elena <rhapsodyenmauve@gmail.com>
2021-08-24 23:12:42 +0100

    testing fs.mkdir

---
+++ workshops/node-intro/boilerplate.js
@@ -0,0 +1,20 @@
+// 1st way ... DOES NOT WORK --- 
+// cannot use import statement outside a module ⚠ 
+// import { mkdir } from 'fs';
+
+// Creates /tmp/a/apple, regardless of whether `/tmp` and /tmp/a exist.
+// mkdir('/tmp/a/apple', { recursive: true }, (err) => {
+//   console.log('i*m in the callback!');
+//   if (err) throw err;
+// });
+// console.log('I come after the mkdir in the file!!');
+
+// --------------------------------------------------------------------------
+// 2nd way ...
+const fs = require('fs');
+fs.mkdir('/tmp/a/apple', { recursive: true }, (err) => {
+  console.log('i*m in the callback!');
+  if (err) throw err;
+});
+
+console.log('I come after the mkdir in the file!!');
