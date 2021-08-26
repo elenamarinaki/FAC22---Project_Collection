18ffdfe8272e8fbca0e13c9b177a6d406fbd55a8
Elena <rhapsodyenmauve@gmail.com>
2021-08-27 00:45:48 +0100

    npm exports - index.js

--- workshops/npm-intro/app.js
+++ workshops/npm-intro/app.js
@@ -1,3 +1,7 @@
 const math = require('./math');
 console.log(math.add(4, 5));
 console.log(math.square(21));
+
+const cats = require('./shelter');
+
+console.log('require an entire directory', cats);
---
+++ workshops/npm-intro/shelter/index.js
@@ -0,0 +1,9 @@
+const blue = require('./blue');
+const sadie = require('./sadie');
+const janet = require('./janet');
+
+const allCats = [blue, sadie, janet];
+
+console.log(allCats);
+
+module.exports = allCats;
