6c6873aed7090d17d328a0377da55a4f80d448e0
Elena <rhapsodyenmauve@gmail.com>
2021-08-26 23:58:49 +0100

    npm exports

---
+++ workshops/npm-intro/math.js
@@ -0,0 +1,17 @@
+const add = (x, y) => x + y;
+
+const PI = 3.14159;
+
+const square = (x) => x * x;
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+const math = {
+  add: add,
+  PI: PI,
+  square: square,
+};
+
+module.exports = math;
